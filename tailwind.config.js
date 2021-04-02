e9fec26884a1e38ca6c2cbb942ed7fae8e4ef9fc
ebinabo <edgarjohn95@gmail.com>
2021-04-02 06:12:52 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: ["./src/**/*.{ts,tsx}"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require("@tailwindcss/aspect-ratio"),
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/line-clamp"),
+        require("@tailwindcss/typography"),
+    ],
+}
